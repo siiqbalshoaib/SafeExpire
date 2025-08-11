aba11ccc892d5eff67660e2628a64c54cd476353
shoaib iqbal <iqbalshoaib425@gmail.com>
2025-08-11 18:56:48 +0530

    ERROR .ENV SOLVED

--- client/src/components/Linkform.jsx
+++ client/src/components/Linkform.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL;
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const Linkform = () => {
   const [activeTab, setActiveTab] = useState("text");

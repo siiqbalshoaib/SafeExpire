b8b428dab73d149198b034986858227abb1a40af
shoaib iqbal <iqbalshoaib425@gmail.com>
2025-08-15 01:53:29 +0530

    ggggggg

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { registerSW } from '../dev-dist/registerSW.js';
+
 
 import "./index.css";
 import App from "./App.jsx";
@@ -10,4 +10,4 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
-registerSW();
+
